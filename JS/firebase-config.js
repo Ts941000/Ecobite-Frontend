86a9830966c778891d5c3bbf7f844cca9bea76a3
coder98899 <kartikeydixit007@gmail.com>
2026-06-20 04:15:47 +0530

    final bug fixes, razorpay integration and vercel deployment setup

---
+++ JS/firebase-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB9WnwgnHB7VRECKcsos3824C9sqATi8ag",
+  authDomain: "ecobite-hx.firebaseapp.com",
+  projectId: "ecobite-hx",
+  storageBucket: "ecobite-hx.firebasestorage.app",
+  messagingSenderId: "84907421623",
+  appId: "1:84907421623:web:5e4bfe774cc67795738232"
+};
+
+export const firebaseCollections = {
+  users: "users",
+  carts: "carts",
+  listings: "listings",
+  orders: "orders",
+};
