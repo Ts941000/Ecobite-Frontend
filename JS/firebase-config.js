e110401ade3f834fd4a4aab1c2395484a6ce1af1
Tejash Sharma <160579875+Ts941000@users.noreply.github.com>
2026-06-21 06:02:32 +0530

    Add measurementId to Firebase configuration

--- JS/firebase-config.js
+++ JS/firebase-config.js
@@ -5,6 +5,7 @@ export const firebaseConfig = {
   storageBucket: "ecobite-hx.firebasestorage.app",
   messagingSenderId: "84907421623",
   appId: "1:84907421623:web:5e4bfe774cc67795738232"
+  measurementId: "G-L3GQ3WXDH7",
 };
 
 export const firebaseCollections = {
